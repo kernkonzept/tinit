2a4d99583956ca34d1f7329d35695bee524c5841
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-03-14 00:00:00 +0000

    Initial import
    System initialization component for safety- and/or resource constrained
    systems.
    
    Change-Id: I37589f3ededc71c62c17c6d92a733c4cc34ef627

---
+++ server/src/debug.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022-2024 Kernkonzept GmbH.
+ * Author(s): Jan Klötzke <jan.kloetzke@kernkonzept.com>
+ *
+ * License: see LICENSE.spdx (in this directory or the directories above)
+ */
+
+#include <l4/sys/debugger.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "debug.h"
+
+int Debug::printf_impl(char const *fmt, ...)
+{
+  int n;
+  va_list args;
+
+  va_start    (args, fmt);
+  n = vdprintf (1, fmt, args);
+  va_end      (args);
+
+  return n;
+}
+
+int Fatal::abort(char const *msg)
+{
+  write(2, "FATAL: ", 7);
+  write(2, msg, strlen(msg));
+  _exit(1);
+}
+
+#ifndef NDEBUG
+void l4_debugger_set_object_name(l4_cap_idx_t cap, cxx::String const &name)
+{
+  char dbg[16];
+  memset(dbg, 0, sizeof(dbg));
+  memcpy(&dbg, name.start(), (unsigned)name.len() >= sizeof(dbg)
+                             ? sizeof(dbg) - 1 : name.len());
+  l4_debugger_set_object_name(cap, dbg);
+}
+
+void l4_debugger_add_image_info(l4_cap_idx_t cap, l4_addr_t base,
+                                cxx::String const &name)
+{
+  char dbg[16];
+  memset(dbg, 0, sizeof(dbg));
+  memcpy(&dbg, name.start(), (unsigned)name.len() >= sizeof(dbg)
+                             ? sizeof(dbg) - 1 : name.len());
+  l4_debugger_add_image_info(cap, base, dbg);
+}
+#endif
